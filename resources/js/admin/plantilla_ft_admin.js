93cff5cf7fecfbaf1f5be406d709a2113b1c1665
Apenater <edgar.aripenate@gmail.com>
2024-03-01 10:31:17 -0600

    Mejoras en admin!!!

--- resources/js/admin/plantilla_ft_admin.js
+++ resources/js/admin/plantilla_ft_admin.js
@@ -1,7 +1,7 @@
 const FOOTER = document.querySelector('footer');
 
 FOOTER.innerHTML = `
-<div class="pie-pagina">
+<div class="pie-pagina fixed-bottom">
 <div class="grupo-1">
     <div class="box">
         <figure>
---
+++ resources/js/admin/plantilla_nav_admin.js
@@ -0,0 +1,26 @@
+const HEADER = document.querySelector('header');
+
+
+HEADER.innerHTML = `
+
+        <nav class="navbar navbar-expand-lg bg-black justify-content-end fixed-top" >
+            <div class="container">
+                <a class="navbar-brand" href="#" style="color: white;">GADGETSIT</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+                    aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end " id="navbarNavDropdown">
+                    <ul class="navbar-nav pr-3">
+                        <li class="nav-item"> <a class="nav-link" href="../../views/admin/productos.html" style="color: white;">Productos</a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="../../views/admin/categorias.html" style="color: white;">Categoria</a>
+                        <li class="nav-item"> <a class="nav-link" href="../../views/admin/marca.html" style="color: white;">Marcas</a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="../../views/admin/promociones.html" style="color: white;">Promociones</a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="../../views/admin/usuarios.html" style="color: white;">Usuarios</a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="../../views/admin/comentarios.html" style="color: white;">Comentarios</a> </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
+        `;
