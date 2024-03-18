b8bf8bd34d426e23f675e3a31efe666f8df26c12
Apenater <edgar.aripenate@gmail.com>
2024-03-18 13:20:23 -0600

    Diseños

--- resources/js/public/plantilla_menu.js
+++ resources/js/public/plantilla_menu.js
@@ -1,80 +1,39 @@
 const HEADER = document.querySelector('header');
 HEADER.innerHTML = `
 
-<div class="header fixed-top ">
-    <div class="uno ">
-        <div class="dos-track">
-            <div class="tres">
-                <p>50% en cel</p>
-            </div>
-            <div class="tres">
-                <p>50% en cel</p>
-            </div>
-            <div class="tres">
-                <p>Texto 3</p>
-            </div>
-            <div class="tres">
-                <p>Texto 4</p>
-            </div>
-            <div class="tres">
-                <p>Texto 5</p>
-            </div>
-            <div class="tres">
-                <p>Texto 6</p>
-            </div>
-            <div class="tres">
-                <p>Texto 1</p>
-            </div>
-            <div class="tres">
-                <p>Texto 2</p>
-            </div>
-            <div class="tres">
-                <p>Texto 3</p>
-            </div>
-            <div class="tres">
-                <p>Texto 4</p>
-            </div>
-            <div class="tres">
-                <p>Texto 5</p>
-            </div>
-            <div class="tres">
-                <p>Texto 6</p>
-            </div>
-        </div>
+<nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+<div class="container-fluid" >
+    <a class="navbar-brand" href="index.html">GADGETSIT</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="productos.html">Todo</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="index.html">Categorias</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="marcas.html">Marcas</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="historial_pedidos.html">Pedidos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="nosotros.html">Acerca de Nosotros</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="login.html">Iniciar
+                    Sesión</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="carrito.html"><img src="../../resources/img/carrito.svg" alt=""></a>
+            </li>
+        </ul>
     </div>
-
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.html">GADGETSIT</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="index.html">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="marcas.html">Marcas</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="historial_pedidos.html">Pedidos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="nosotros.html">Acerca de Nosotros</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="login.html">Iniciar
-                            Sesión</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="carrito.html"><img src="../../resources/img/carrito.svg" alt=""></a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
 </div>
-
+</nav>
 `;
