94c71e596c5731dc98425ef51e75b282a882ed50
Apenater <107435515+Apenater@users.noreply.github.com>
2024-03-18 14:15:31 -0600

    S
    S

--- resources/js/admin/plantilla_ft_admin.js
+++ resources/js/admin/plantilla_ft_admin.js
@@ -1,15 +1,18 @@
+// Selecciona el elemento footer en el documento HTML
 const FOOTER = document.querySelector('footer');
 
+// Asigna contenido HTML al footer
 FOOTER.innerHTML = `
 <div class="pie-pagina bottom">
-<div class="grupo-1">
-    <div class="box">
-        <figure>
-            <a href="#">
-                <img src="../../resources/img/logo.svg" alt="Logo de SLee Dw">
-            </a>
-        </figure>
+    <div class="grupo-1">
+        <div class="box">
+            <figure>
+                <a href="#">
+                    <!-- Inserta una imagen como enlace -->
+                    <img src="../../resources/img/logo.svg" alt="Logo de SLee Dw">
+                </a>
+            </figure>
+        </div>
     </div>
-</div>
 </div>
 `;
