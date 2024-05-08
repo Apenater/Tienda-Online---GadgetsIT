feb1eb9b6bc0bd68534b35662e1bfdf3ac09270b
Andres:D <20210549@ricaldone.edu.sv>
2024-05-08 09:56:44 -0600

    crud marcas...

---
+++ api/models/data/marca_data.php
@@ -0,0 +1,83 @@
+<?php
+// Se incluye la clase para validar los datos de entrada.
+require_once('../../helpers/validator.php');
+// Se incluye la clase padre.
+require_once('../../models/handler/marcas_handler.php');
+/*
+ *  Clase para manejar el encapsulamiento de los datos de la tabla MARCA.
+ */
+class MarcaData extends MarcaHandler
+{
+    /*
+     *  Atributos adicionales.
+     */
+    private $data_error = null;
+    private $filename = null;
+
+    /*
+     *  Métodos para validar y establecer los datos.
+     */
+    public function setId($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->id_marca = $value;
+            return true;
+        } else {
+            $this->data_error = 'El identificador de la marca es incorrecto';
+            return false;
+        }
+    }
+
+    public function setNombre($value, $min = 2, $max = 50)
+    {
+        if (!Validator::validateAlphanumeric($value)) {
+            $this->data_error = 'El nombre debe ser un valor alfanumérico';
+            return false;
+        } elseif (Validator::validateLength($value, $min, $max)) {
+            $this->nombre_marca = $value;
+            return true;
+        } else {
+            $this->data_error = 'El nombre debe tener una longitud entre ' . $min . ' y ' . $max;
+            return false;
+        }
+    }
+
+    public function setImagen($file, $filename = null)
+    {
+        if (Validator::validateImageFile($file, 200)) {
+            $this->foto_marca = Validator::getFilename();
+            return true;
+        } elseif (Validator::getFileError()) {
+            $this->data_error = Validator::getFileError();
+            return false;
+        } elseif ($filename) {
+            $this->foto_marca = $filename;
+            return true;
+        } else {
+            $this->foto_marca = 'default.png';
+            return true;
+        }
+    }
+    public function setFilename()
+    {
+        if ($data = $this->readFilename()) {
+            $this->filename = $data['foto'];
+            return true;
+        } else {
+            $this->data_error = 'Categoría inexistente';
+            return false;
+        }
+    }
+    /*
+     *  Métodos para obtener los atributos adicionales.
+     */
+    public function getDataError()
+    {
+        return $this->data_error;
+    }
+
+    public function getFilename()
+    {
+        return $this->filename;
+    }
+}
