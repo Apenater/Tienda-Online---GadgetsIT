4a9930a5952331621ebb95e3c22871fb8bfcd76e
Apenater <edgar.aripenate@gmail.com>
2024-05-08 09:16:34 -0600

    Avance

---
+++ api/models/data/productos_data.php
@@ -0,0 +1,101 @@
+<?php
+// Se incluye la clase para validar los datos de entrada.
+require_once('../../helpers/validator.php');
+// Se incluye la clase padre.
+require_once('../../models/handler/productos_handler.php');
+/*
+ *  Clase para manejar el encapsulamiento de los datos de la tabla CATEGORIA.
+ */
+class ProductosData extends ProductosHandler
+{
+    /*
+     *  Atributos adicionales.
+     */
+    private $data_error = null;
+    private $filename = null;
+
+    /*
+     *  Métodos para validar y establecer los datos.
+     */
+    public function setId($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->id_Categoria = $value;
+            return true;
+        } else {
+            $this->data_error = 'El identificador de el producto es incorrecto';
+            return false;
+        }
+    }
+
+    public function setNombre($value, $min = 2, $max = 50)
+    {
+        if (!Validator::validateAlphanumeric($value)) {
+            $this->data_error = 'El nombre debe ser un valor alfanumérico';
+            return false;
+        } elseif (Validator::validateLength($value, $min, $max)) {
+            $this->nombreC = $value;
+            return true;
+        } else {
+            $this->data_error = 'El nombre debe tener una longitud entre ' . $min . ' y ' . $max;
+            return false;
+        }
+    }
+
+    public function setImagen($file, $filename = null)
+    {
+        if (Validator::validateImageFile($file, 200)) {
+            $this->foto = Validator::getFilename();
+            return true;
+        } elseif (Validator::getFileError()) {
+            $this->data_error = Validator::getFileError();
+            return false;
+        } elseif ($filename) {
+            $this->foto = $filename;
+            return true;
+        } else {
+            $this->foto = 'default.png';
+            return true;
+        }
+    }
+
+    public function setDescripcion($value, $min = 2, $max = 250)
+    {
+        if (!$value) {
+            return true;
+        } elseif (!Validator::validateString($value)) {
+            $this->data_error = 'La descripción contiene caracteres prohibidos';
+            return false;
+        } elseif (Validator::validateLength($value, $min, $max)) {
+            $this->Descripcion = $value;
+            return true;
+        } else {
+            $this->data_error = 'La descripción debe tener una longitud entre ' . $min . ' y ' . $max;
+            return false;
+        }
+    }
+
+    public function setFilename()
+    {
+        if ($data = $this->readFilename()) {
+            $this->filename = $data['imagen_categoria'];
+            return true;
+        } else {
+            $this->data_error = 'Categoría inexistente';
+            return false;
+        }
+    }
+
+    /*
+     *  Métodos para obtener los atributos adicionales.
+     */
+    public function getDataError()
+    {
+        return $this->data_error;
+    }
+
+    public function getFilename()
+    {
+        return $this->filename;
+    }
+}
