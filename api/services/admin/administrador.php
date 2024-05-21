80803a8c4e123b07ce45854f1eb5bd71fb7fe926
Andres:D <20210549@ricaldone.edu.sv>
2024-05-21 09:20:13 -0600

    el editar perfil
    el editar perfil ya furula

--- api/services/admin/administrador.php
+++ api/services/admin/administrador.php
@@ -11,7 +11,7 @@ if (isset($_GET['action'])) {
     // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
     $result = array('status' => 0, 'session' => 0, 'message' => null, 'dataset' => null, 'error' => null, 'exception' => null, 'username' => null);
     // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
-    if (isset($_SESSION['id_admin']) ) {
+    if (isset($_SESSION['id_admin'])) {
         $result['session'] = 1;
         // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
         switch ($_GET['action']) {
@@ -116,16 +116,16 @@ if (isset($_GET['action'])) {
             case 'editProfile':
                 $_POST = Validator::validateForm($_POST);
                 if (
-                    !$administrador->setNombre($_POST['nombreAdministrador']) or
-                    !$administrador->setApellido($_POST['apellidoAdministrador']) or
-                    !$administrador->setCorreo($_POST['correoAdministrador']) or
-                    !$administrador->setTelefono($_POST['telefono'])
+                    !$administrador->setNombre($_POST['nombreEmpleado']) or
+                    !$administrador->setApellido($_POST['apellidoEmpleado']) or
+                    !$administrador->setCorreo($_POST['correoEmpleado']) or
+                    !$administrador->setTelefono($_POST['telefonoEmpleado'])
                 ) {
                     $result['error'] = $administrador->getDataError();
                 } elseif ($administrador->editProfile()) {
                     $result['status'] = 1;
                     $result['message'] = 'Perfil modificado correctamente';
-                    $_SESSION['correo'] = $_POST['correo'];
+                    $_SESSION['correo'] = $_POST['correoEmpleado'];
                 } else {
                     $result['error'] = 'Ocurrió un problema al modificar el perfil';
                 }
