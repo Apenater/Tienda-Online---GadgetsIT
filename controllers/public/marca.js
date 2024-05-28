0e87b186d1da44e52eb954d21adfee0c558d6e40
Andres:D <20210549@ricaldone.edu.sv>
2024-05-28 11:50:53 -0600

    YA SE MUETRSN MARCAS
    YA SE MUETRSN MARCAS

---
+++ controllers/public/marca.js
@@ -0,0 +1,36 @@
+// Constante para completar la ruta de la API.
+const CATEGORIA_API = 'services/public/marca.php';
+// Constante para establecer el contenedor de categorías.
+const MARCAS = document.getElementById('marca');
+
+// Método del evento para cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', async () => {
+    // Llamada a la función para mostrar el encabezado y pie del documento.
+
+    // Se establece el título del contenido principal.
+    // Petición para obtener las categorías disponibles.
+    const DATA = await fetchData(CATEGORIA_API, 'readAll');
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (DATA.status) {
+        // Se inicializa el contenedor de categorías.
+        MARCAS.innerHTML = '';
+        // Se recorre el conjunto de registros fila por fila a través del objeto row.
+        DATA.dataset.forEach(row => {
+            // Se establece la página web de destino con los parámetros.
+            let url = `products.html?id=${row.id_categoria}&nombre=${row.nombre_categoria}`;
+            // Se crean y concatenan las tarjetas con los datos de cada categoría.
+            MARCAS.innerHTML += `
+                
+
+
+                <a href="productos.html">
+                <div class="negro">
+                <img src="${SERVER_URL}images/marcas/${row.imagen_marca}" alt="Image-ct">
+                </div>
+              </a>
+            `;
+        });
+    } else {
+        // Se asigna al título del contenido de la excepción cuando no existen datos para mostrar.
+    }
+});
