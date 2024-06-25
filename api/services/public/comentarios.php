a5059ab20e6a0b5009128e699b33402a1f5b02f2
Rolin117 <kvnrolin@gmail.com>
2024-06-25 07:24:45 -0600

    resolviendo conflictos

--- api/services/public/comentarios.php
+++ api/services/public/comentarios.php
@@ -2,6 +2,46 @@
 require_once('../../models/data/comentarios_data.php');
 
 if (isset($_GET['action'])) {
+    session_start();
+    $comentario = new ComentariosData();
+    $result = array('status' => 0, 'message' => null, 'dataset' => null, 'error' => null, 'exception' => null);
+
+    if (isset($_SESSION['id_admin'])) {
+        switch ($_GET['action']) {
+            case 'readAll':
+                // Recibir el ID del producto desde GET
+                if (!isset($_GET['idProducto']) || !$comentario->setProductoId($_GET['idProducto'])) {
+                    $result['error'] = 'ID de producto inválido';
+                } elseif ($result['dataset'] = $comentario->readAllByProductId($_GET['idProducto'])) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Existen ' . count($result['dataset']) . ' comentarios para este producto';
+                } else {
+                    $result['error'] = 'No existen comentarios registrados para este producto';
+                }
+                break;
+            case 'hide':
+                if (!$comentario->setId($_POST['idComentario'])) {
+                    $result['error'] = $comentario->getDataError();
+                } elseif ($comentario->hideComment()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Comentario ocultado correctamente';
+                } else {
+                    $result['error'] = 'Ocurrió un problema al ocultar el comentario';
+                }
+                break;
+            default:
+                $result['error'] = 'Acción no disponible dentro de la sesión';
+                break;
+        }
+        $result['exception'] = Database::getException();
+    } else {
+        $result['error'] = 'Acceso denegado';
+    }
+
+    header('Content-type: application/json; charset=utf-8');
+    echo json_encode($result);
+} else {
+    echo json_encode(array('error' => 'Recurso no disponible'));
     $comentario = new ComentariosData;
     $result = array('status' => 0, 'message' => null, 'dataset' => null, 'error' => null, 'exception' => null);
 
